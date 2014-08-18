855d13baa92ccad71bb6e01a7172ba2b6eff333b
RaspberryPi <kenchou77@gmail.com>
2014-08-18 21:38:03 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+update peanuthull DDNS
+======================
+
+ Usage:
+     ph-ddns.sh [config]
+
+Configuration
+-------------
+IP_URL 获取公网IP的服务
+AUTHROIZATION 花生壳的身份认证 Key，username:password 做 base64 encode
+DEBUG 非0输出更多的信息
---
+++ ph-ddns.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Update peanuthull DDNS
+# author: Ken Chou <kenchou77@gmail.com>
+# Usage:
+#    ph-ddns.sh
+#    ph-ddns.sh another/config
+
+BIN_PATH=`dirname "$0"`
+CONFIG_FILE=${1:-$BIN_PATH/config}
+# include config file
+. $CONFIG_FILE
+
+CMD_GET_IP="curl -s $IP_URL"
+[[ $DEBUG -ne 0 ]] && echo $CMD_GET_IP
+
+IP=`$CMD_GET_IP`
+[ $? -ne 0 ] && echo "Oops. Cannot get extenal IP!" && exit 1
+
+if [[ ( -f "$OLD_IP" ) && ( "$IP" = "`cat $OLD_IP`" ) ]]
+then
+    # IP No Change.
+    echo "$IP IP No change."
+    exit 0
+fi
+
+# update DDSN
+echo "Run @ `date '+%F %R:%S'`"
+echo "Updating DDNS... Current IP: $IP"
+
+AUTH_HEADER="Authorization: Basic ${AUTHORIZATION}"
+CMD_UPDATE_DDNS=( curl -s -H "${AUTH_HEADER}" -A "${USERAGENT}" "${DDNS_UPDATE_URL}${IP}" )
+[[ $DEBUG -ne 0 ]] && echo ${CMD_UPDATE_DDNS[@]}
+
+RESULT=`"${CMD_UPDATE_DDNS[@]}"`
+[ $? -ne 0 ] && echo "Update Error." $RESULT && exit 1 || echo $RESULT
+
+# save current ip
+echo $IP > $OLD_IP
